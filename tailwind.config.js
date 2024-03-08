2a1f747449661af8259ebe9aca709552c4b2a43f
Md. Shariful Haque <shagor.shariful619@gmail.com>
2024-03-08 14:25:44 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./core/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        primary: ["Montserrat", "sans-serif"],
+      }
+    },
+  },
+  plugins: [],
+}
+
